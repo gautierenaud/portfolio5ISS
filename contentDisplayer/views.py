17d89b77b4b4edf354190b73d9342113e79b260b
gautierenaud <gautierenaud@gmail.com>
2017-01-04 12:25:29 +0100

    add list of related models for each article

--- contentDisplayer/views.py
+++ contentDisplayer/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from .models import Content
-
+from .models import Article, Section, Document, Image
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 # Create your views here.
 
 def content(request, contentId):
-	contentToDisplay = Content.objects.get(id=contentId)
-	return render(request, 'contentDisplayer/content.html', {'contentToDisplay' : contentToDisplay})
+	contentToDisplay = Article.objects.get(id=contentId)
+	sections = Section.objects.filter(parentArticle__id=contentId).order_by('order')
+	images = Image.objects.filter(parentArticle__id=contentId)
+	imageCount = len(images)
+	documents = Document.objects.filter(parentArticle__id=contentId)
+	docCount = len(documents)
+	
+	return render(request, 'contentDisplayer/content.html', {'contentToDisplay' : contentToDisplay, 'sections' : sections, 'images' : images, 'imageCount' : imageCount, 'documents' : documents, 'docCount' : docCount})
 
 def contentList(request):
-	contents = Content.objects.all()
+	contents = Article.objects.all()
 	return render(request, 'contentDisplayer/contentList.html', {'contents' : contents})
