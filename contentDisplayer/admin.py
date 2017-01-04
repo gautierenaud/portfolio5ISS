8c1a7384ff797d11d4e7c2e31139c43bdf39038e
gautierenaud <gautierenaud@gmail.com>
2017-01-04 12:21:23 +0100

    add models to admin console

--- contentDisplayer/admin.py
+++ contentDisplayer/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Content, Tag
+from .models import Article, Tag, Section, Document, Image
 
 # Register your models here.
 
-admin.site.register(Content)
+admin.site.register(Article)
 admin.site.register(Tag)
+admin.site.register(Section)
+admin.site.register(Document)
+admin.site.register(Image)
